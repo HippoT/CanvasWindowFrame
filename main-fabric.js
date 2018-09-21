29e3adb7ac683210e2746182bb1418aa8fc47c6b
HippoT <minhtuan.02071995@gmail.com>
2018-09-21 11:15:02 +0700

    Create frame by fabric

--- main-fabric.js
+++ main-fabric.js
@@ -1,42 +1,78 @@
+var width = window.innerWidth - 500;
+var height = window.innerHeight;
+$("#contentWindowFrame").append('<canvas style="border: 1px solid black" id="canvasWindowFrame" width="' + width + '" height="' + height + '"></canvas>')
+
 function test() {
-    var canvas = this.__canvas = new fabric.Canvas('contentWindowFrame');
-  
-    var startPoints = [
-      {x: 0, y: 0},
-      {x: 155, y: 0},
-      {x: 155, y: 243},
-      {x: 0, y: 243},
-      {x: 0, y: 0},
+    var canvas = this.__canvas = new fabric.Canvas('canvasWindowFrame');
 
-      {x: 5, y: 5},
-      {x: 150, y: 5},
-      {x: 150, y: 238},
-      {x: 5, y: 238},
-      {x: 5, y: 5},
+    var groupFrame = CreateFrame(200, 200, 30);
+    canvas.add(groupFrame);
+}
 
-      {x: 150, y: 5},
-      {x: 155, y: 0},
-      {x: 155, y: 243},
-      {x: 150, y: 238},
-      {x: 5, y: 238},
-      {x: 0, y: 243}
+function CreateFrame(frameWidth, frameHeight, frameProfile){
+  
 
-    ];
+  var fillColor = "white";
+  //top
+  var topPoints = [
+    {x : 0, y: 0},
+    {x : frameWidth, y : 0},
+    {x : frameWidth - frameProfile, y: frameProfile},
+    {x : frameProfile, y: frameProfile}
+  ]
+  var topPolygon = CreatePolygon(topPoints, fillColor, {x: 0, y: 0});
+
+  //Left
+  var leftPoints=[
+    {x : 0, y : 0},
+    {x : frameProfile, y : frameProfile},
+    {x : frameProfile, y :frameHeight - frameProfile},
+    {x : 0, y : frameHeight}
+  ]
+  var leftPolygon = CreatePolygon(leftPoints, fillColor, {x : 0, y : 0});
+
+  //botton
+  var botPoints = [
+    { x: 0, y: frameHeight },
+    { x: frameProfile, y: frameHeight - frameProfile },
+    { x: frameWidth - frameProfile, y: frameHeight - frameProfile },
+    { x: frameWidth, y: frameHeight }
+  ]
+  var botPolygon = CreatePolygon(botPoints, fillColor, {x : 0, y : frameHeight - frameProfile});
+
+  //right
+  var rightPoints = [
+    {x : frameWidth, y : 0},
+    {x : frameWidth, y : frameHeight},
+    {x : frameWidth - frameProfile, y : frameHeight - frameProfile},
+    {x : frameWidth - frameProfile, y : frameProfile}
+  ];
+  var rightPolygon = CreatePolygon(rightPoints, fillColor, {x : frameWidth - frameProfile, y : 0})
+
   
-    var clonedStartPoints = startPoints.map(function(o){
-      return fabric.util.object.clone(o);
-    });
-  
-    var polygon = new fabric.Polygon(clonedStartPoints, {
-      left: 10,
-      top: 10,
-      fill: 'purple',
-      selectable: true,
-      objectCaching: false,
-      strokeWidth: 0.5, 
-      stroke: 'black'
-    });
-    canvas.add(polygon);
+  var group = new fabric.Group([topPolygon, leftPolygon, botPolygon, rightPolygon], {
+    left: 0,
+    top: 0
+  });
+  return group;
+}
+
+function CreatePolygon(points, fillColor, startPoint){
+  var clonedPoints = points.map(function(o){
+    return fabric.util.object.clone(o);
+  });
+
+  var polygon = new fabric.Polygon(clonedPoints, {
+    left: startPoint.x,
+    top: startPoint.y,
+    fill: fillColor,
+    selectable: true,
+    objectCaching: false,
+    strokeWidth: 0.5, 
+    stroke: 'black'
+  });
+
+  return polygon;
 }
 
 test();
